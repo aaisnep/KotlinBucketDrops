cf002b057898e091508df823d3ee0ed0fdd8eddb
aaisnep <aaisnep@gmail.com>
2016-10-22 22:09:09 +0200

    hhaha

--- app/src/main/java/nl/rekenbinair/h/kotlinbucketdrops/ActivityMain.java
+++ app/src/main/java/nl/rekenbinair/h/kotlinbucketdrops/ActivityMain.java
@@ -1,13 +1,16 @@
 package nl.rekenbinair.h.kotlinbucketdrops;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class ActivityMain extends AppCompatActivity {
 
+    ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
